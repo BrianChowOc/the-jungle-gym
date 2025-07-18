c498c9c9b6b14bc8175fa49b2bbf5e9ae86cc106
cbrian <brianchow26@gmail.com>
2025-07-18 22:52:23 +0200

    feat: test page

--- terraform/modules/database/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment" {}
-variable "instance_class" { default = "db.t2.micro" }
-variable "storage"        { default = 20 }
-variable "db_name"        {}
-variable "username"       {}
-variable "password"       {}
-variable "security_group_id" {}
-variable "db_subnet_group_name" {}
