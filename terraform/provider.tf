0a86628a2782de3be2359ee452f458844625b12c
cbrian <brianchow26@gmail.com>
2025-07-19 11:31:07 +0200

    feat: new repot

--- terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-# =====================================
-# Fichier provider.tf
-# ➔ Déclare le cloud provider utilisé (AWS)
-# =====================================
-
-provider "aws" {
-  region = "eu-west-3"
-}
